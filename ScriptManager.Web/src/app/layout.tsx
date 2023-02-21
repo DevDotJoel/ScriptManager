b1668e66b4a789f8cd1d3ac853add1a06ca6a951
Joel Ferreira <30007769@students.ual.pt>
2023-02-21 20:22:12 +0000

    oijop

--- ScriptManager.Web/src/app/layout.tsx
+++ ScriptManager.Web/src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
         <div className="container-fluid p-0">
           <div className="container-fluid">
             <div className="row flex-nowrap">
-              <div className="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark">
+              <div className="col-auto col-md-3 col-xl-2 px-sm-2 px-0 custom-dark">
                 <div className="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
                   <Link
                     href="/"
