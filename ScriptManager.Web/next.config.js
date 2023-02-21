2f9dcfae2b137ae87741f614a68ea5398bfd5b72
Joel Ferreira <30007769@students.ual.pt>
2023-02-21 17:27:41 +0000

    aed

--- ScriptManager.Web/next.config.js
+++ ScriptManager.Web/next.config.js
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output:"standalone",
-  reactStrictMode: true,
   experimental: {
     appDir: true,
     serverComponentsExternalPackages: ['react-bootstrap'],  },
